1c959e6e738f59eaaa1dfe74c5ac142b1b849025
bUNYrAbbito <singhbunny116@gmail.com>
2024-07-29 18:21:36 +0530

    ok

--- src/compo/Nav.js
+++ src/compo/Nav.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Nav(){
     return(
         <div>
-            <img src={image}  alt="profile image"></img>
+            <img src={image}  alt="profile image"/>
             <h1>prasant singh</h1>
             <p>I Am a full stack developer working on various projects, including React, Angular, and a simplified web application for note-taking. If there's anything more you'd like to add or if you need help with any specific aspect of your projects, feel free to let me know!</p>
             <button>contect</button>
